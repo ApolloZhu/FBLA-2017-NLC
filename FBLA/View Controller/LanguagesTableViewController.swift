13832198dd7a952f37bca8092722438d0f26d3dd
Apollo Zhu <public-apollonian@outlook.com>
2017-02-06 07:13:26 -0500

    Some locale implementation

--- FBLA/View Controller/LanguagesTableViewController.swift
+++ FBLA/View Controller/LanguagesTableViewController.swift
@@ -17,13 +17,39 @@ class LanguagesTableViewController: UITableViewController {
     lazy var localizations: [String] = {
         return Bundle.main.localizations.filter { $0 != "Base" }
     }()
+    static let translators = ["Apollo Zhu", "Khan Mujtaba", "Seonuk Kim"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        _ = Locale.current.localizedString(forLanguageCode: localizations[0])
+    }
 
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == 0 {
+            return localizations.count
+        } else {
+            return LanguagesTableViewController.translators.count
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")!
+        if indexPath.section == 0 {
+            cell.textLabel?.text = localizations[indexPath.row]
+        } else {
+            cell.textLabel?.text = LanguagesTableViewController.translators[indexPath.row]
+        }
+        return cell
     }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
+        if indexPath.section == 0 {
+            UserDefaults.standard.set([localizations[indexPath.row]], forKey: "AppleLanguages")
+            UserDefaults.standard.synchronize()
+        }
     }
 }
